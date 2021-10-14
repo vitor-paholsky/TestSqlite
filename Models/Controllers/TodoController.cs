10abedaea05467b862667061b0e7044fc5909764
Vitor Paholsky <vitor_paduarte@hotmail.com>
2021-10-14 17:20:57 -0300

    Initial commit

---
+++ Models/Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuTodo.Models.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("todos")]
+        public List<Todo> Get()
+        {
+            return new List<Todo>();
+        }
+    }
+}
